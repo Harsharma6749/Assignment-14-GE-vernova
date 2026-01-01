a8e0f648b40a9b942af9b0da53a42d5b89d62a22
Harsharma6749 <harshsharma6749@gmail.com>
2026-01-01 11:04:51 +0530

    Create q7_prime.java

---
+++ q7_prime.java
@@ -0,0 +1,14 @@
+public class q7_prime {
+
+    static boolean prime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= n / 2; i++)
+            if (n % i == 0) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 1000; i++)
+            if (prime(i)) System.out.print(i + " ");
+    }
+}
