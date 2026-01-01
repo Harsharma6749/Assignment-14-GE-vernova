d2c2ecf5e64e41f2c3c397e83c24711b3823fc7e
Harsharma6749 <harshsharma6749@gmail.com>
2026-01-01 11:03:46 +0530

    Create  q4_bubble.java

---
+++ q4_bubble.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class q4_bubble {
+
+    public static void main(String[] args) {
+        int[] a = {5, 2, 8, 1};
+
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < a.length - 1; j++)
+                if (a[j] > a[j + 1]) {
+                    int t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+                }
+
+        System.out.println(Arrays.toString(a));
+    }
+}
